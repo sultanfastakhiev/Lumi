073355be62e8be10d1c3c0e49d5572c619582b8f
zotovy <the1ime@yandex.ru>
2022-10-15 09:46:59 +0300

    feat(web): add empty state to patients

--- web/src/feats/patients/pages/patients-list/patients-list.tsx
+++ web/src/feats/patients/pages/patients-list/patients-list.tsx
@@ -4,7 +4,7 @@ import { Layout } from "@core/components/layout/layout";
 import { ContentWrapper } from "@core/components/content-wrapper/content-wrapper";
 import { Title } from "@core/components/typography/title/title";
 import { Button } from "react-untitled-ui";
-import { Plus } from "react-feather";
+import { Plus, Search } from "react-feather";
 import { usePatientsList } from "@feats/patients/pages/patients-list/use-patients-list";
 import { LoadingView } from "@core/components/loading/loading-view/loading-view";
 import { PatientCard } from "@feats/patients/components/patient-card/patient-card";
@@ -23,19 +23,36 @@ export const PatientsListPage: React.FC = () => {
             isLoading
                 ? <LoadingView/>
                 : <ContentWrapper>
-                    <Title
-                        suffix={ <Button 
-                            onClick={() => navigate("/apps/patients/create")}
-                            trailingIcon={ Plus }
-                            variant="secondary-gray">Создать пациента</Button> }>
-                        Мои пациенты
-                    </Title>
-                    <div className={ styles.grid }>
-                        {
-                            data!.map(patient => <PatientCard patient={ patient } key={ patient.id }/>)
-                        }
-                    </div>
+                    {
+                        data!.length === 0
+                            ? <EmptyState/>
+                            : <React.Fragment>
+                                <Title
+                                    suffix={ <Button
+                                        onClick={ () => navigate("/apps/patients/create") }
+                                        trailingIcon={ Plus }
+                                        variant="secondary-gray">Создать пациента</Button> }>
+                                    Мои пациенты
+                                </Title>
+                                <div className={ styles.grid }>
+                                    {
+                                        data!.map(patient => <PatientCard patient={ patient } key={ patient.id }/>)
+                                    }
+                                </div>
+                            </React.Fragment>
+                    }
                 </ContentWrapper>
         }
     </Layout>
+}
+
+export const EmptyState: React.FC = () => {
+    const navigate = useNavigate()
+
+    return <div className={ styles.emptyState }>
+        <Search className={ styles.icon }/>
+        <h3 className={ styles.title }>Пациентов не найдено</h3>
+        <span className={ styles.subtitle }>Нажмите на кнопку чтобы<br/>вашего первого пациента</span>
+        <Button onClick={ () => navigate("/apps/patients/create") }>Создать пациента</Button>
+    </div>
 }
