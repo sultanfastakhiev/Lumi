d112958bdfaff4879879970cac557f75d14076cf
zotovy <the1ime@yandex.ru>
2022-10-29 01:55:14 +0500

    feat(mobile): implement in app review

--- mobile/lib/feats/account/screens/about_app_screen.dart
+++ mobile/lib/feats/account/screens/about_app_screen.dart
@@ -1,14 +1,24 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:mobile/components/header.dart';
 import 'package:mobile/components/layouts/empty.dart';
 import 'package:mobile/feats/account/components/app_version.dart';
 import 'package:mobile/feats/account/components/secondary_menu_tile.dart';
 import 'package:mobile/router/router.gr.dart';
+import 'package:mobile/utils/utils.dart';
 
 class AboutAppScreen extends StatelessWidget {
   const AboutAppScreen({Key? key}) : super(key: key);
+
+  Future<void> reviewApp(BuildContext context) async {
+    if (await InAppReview.instance.isAvailable()) {
+      InAppReview.instance.requestReview();
+    } else {
+      showError(context, "In app review не доступно на данном устройстве");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +47,7 @@ class AboutAppScreen extends StatelessWidget {
             ),
             SecondaryMenuTile(
               text: "Оценить приложение",
-              onTap: () {},
+              onTap: () => reviewApp(context),
             ),
             SecondaryMenuTile(
               text: "Политика конфиденциальности",
--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import in_app_review
 import package_info_plus
 import path_provider_macos
 import sentry_flutter
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
