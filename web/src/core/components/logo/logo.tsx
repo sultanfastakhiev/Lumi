a8225e0e802efcd4911aa0b434d79b48702bb81b
zotovy <the1ime@yandex.ru>
2022-10-29 22:37:21 +0500

    feat(web): replace default img with next/image

--- web/src/core/components/logo/logo.tsx
+++ web/src/core/components/logo/logo.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties } from "react";
 import styles from "./logo.module.scss";
 import classNames from "classnames";
+import Image from "next/image";
 
 export type LogoProps = {
     size?: CSSProperties["width"] | CSSProperties["height"];
@@ -12,7 +13,7 @@ export const Logo: React.FC<LogoProps> = React.memo((props) => {
     const {size = 32} = props;
 
     return <div className={ classNames(props.className, styles.logo) } style={ {width: size, height: size} }>
-        <img src="/logo.png" alt="Logo"/>
+        <Image width={32} height={32} src="/logo.png" alt="Logo"/>
         {
             props.text && <span className={ styles.text }>Lumi</span>
         }
